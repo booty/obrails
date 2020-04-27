ce648ac80da2424dbef59a5ab9db8147ff3dd4c5
booty <johnedmundrose@gmail.com>
2020-04-27 08:36:53 -0400

    Create User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  enum gender: {
+    female: 0,
+    male: 1,
+    trans_female: 2,
+    trans_male: 3,
+    nonbinary: 4,
+    fuck_off: 5,
+    let_me_tell_you: 6,
+  }
+
+  enum account_status: {
+    active: 1,
+    retired: 2,
+    banned: 3,
+  }
 end
--- db/migrate/20200421100010_create_users.rb
+++ db/migrate/20200421100010_create_users.rb
@@ -1,17 +1,31 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
-  def change
+  def up
+    execute <<~SQL
+      CREATE TYPE user_gender AS ENUM ('female', 'male', 'trans_female', 'trans_male', 'nonbinary', 'fuck_off', 'let_me_tell_you');
+      CREATE TYPE user_account_status AS ENUM ('active', 'retired', 'banned')
+    SQL
     create_table :users do |t|
-      t.string :nick
+      t.string :type, required: true, index: true
+      t.string :nick, required: true
       t.string :friendlyname
-      t.string :password
-      t.date :dob
-      t.date :dob_fuzzed
-      t.string :gender
+      t.string :password, required: true
+      t.date :dob, required: true
+      t.date :dob_fuzzed, required: true, index: true
+      t.datetime :deleted_at, index: true
       t.string :gender_comment
-      t.datetime :deleted_at
-      t.string :usertype
-
       t.timestamps
+      t.datetime :account_status_updated_at, required: true, default: -> { 'CURRENT_TIMESTAMP' }
+      t.datetime :gender_updated_at, required: true, default: -> { 'CURRENT_TIMESTAMP' }
     end
+    add_column :users, :gender, :user_gender, after: :password, index: true
+    add_column :users, :account_status, :user_account_status, after: :password, index: true
+  end
+
+  def down
+    drop_table :users
+    execute <<~SQL
+      DROP TYPE user_gender;
+      DROP TYPE user_account_status;
+    SQL
   end
 end
